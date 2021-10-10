417d5379a21ccc1a9ce548ebf1b7ef76fd6534fa
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-10-10 11:10:01 +0300

    Update ilmava.js

--- server/ilmava.js
+++ server/ilmava.js
@@ -10,17 +10,22 @@ const router = express.Router();
 app.use(express.json());
 app.use(express.urlencoded({extended: true,}));
 
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS ');
+//     res.header(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With,' +
+//         ' Content-Type, Accept,' +
+//         ' Authorization,' +
+//         ' Access-Control-Allow-Credentials'
+//     );
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header("Access-Control-Allow-Origin", "*");
+//     next();
+// });
+
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS ');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With,' +
-        ' Content-Type, Accept,' +
-        ' Authorization,' +
-        ' Access-Control-Allow-Credentials'
-    );
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Origin', '*');
     next();
 });
 
@@ -28,6 +33,12 @@ app.use(function (req, res, next) {
 router.use(function (req, res, next) {
     console.log(req.method + req.url);
     next();
+});
+
+app.use('/', router);
+router.get('/', async function (req, res) {
+    console.log('default get route called');
+    res.json({status: 'received'});
 });
 
 app.use('/device/v1', router);
