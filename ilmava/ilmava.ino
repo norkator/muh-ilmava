d9395b4685a424267deda602ad1ff0c61c3fc8f5
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-10-10 12:15:21 +0300

    Refactored Arduino side code

--- ilmava/ilmava.ino
+++ ilmava/ilmava.ino
@@ -1,8 +1,8 @@
 /**
- * MUH Ilmava Temperature monitoring screen
+ * MUH Ilmava Temperature measuring and sender device
  * Arduino: Mega2560
  * Temperature sensors: 4 x DHT22
- * + Ethernet Card
+ * + Ethernet Card (NOTE: 50, 51, 52, 53 are somehow used by wiznet)
  *  
  *  Oled screen support
  *  To enable following screen support you need to install libs: "Adafruit BusIO", "Adafruit-GFX-Library" and "Adafruit_SSD1306"
@@ -13,37 +13,29 @@
  *     VDD -> 5v (Screen pin labeled as VCC or VDD)
  *
  */
-// LIBRARIES
+// --------------------------------------------------------------------------------
+// ## Libraries ##
 #include <ArduinoJson.h>
 #include <SPI.h>
 #include <Dhcp.h>
 #include <Dns.h>
 #include <Ethernet.h>
 #include <EthernetClient.h>
+#include <EthernetServer.h>
 #include <EthernetUdp.h>
+#include <ArduinoJson.h>
 #include <dht.h>
+#include <TaskScheduler.h>
 
 // --------------------------------------------------------------------------------
-// ## ETHERNET CONFIG ##
-EthernetClient client;
-byte mac[] = { 0xFA, 0xF4, 0x9A, 0xC7, 0xC6, 0xC2 }; // Mac address for ethernet nic
-const char* server  = "192.168.2.35";
-const int port      = 8080; // API listening port
-const char* api     = "/device/v1/measurements"; // API route
-const unsigned long HTTP_TIMEOUT = 10000; // Timeout for http call
-const size_t MAX_CONTENT_SIZE = 124; // Must be incremented if http response comes out as null
-
+// ## Pins config ##
+#define DHT22_1_PIN 46
+#define DHT22_2_PIN 47
+#define DHT22_3_PIN 48
+#define DHT22_4_PIN 49
 
 // --------------------------------------------------------------------------------
-// ## PINS CONFIGURATION ##
-#define DHT22_1_PIN 50
-#define DHT22_2_PIN 51
-#define DHT22_3_PIN 52
-#define DHT22_4_PIN 53
-
-
-// --------------------------------------------------------------------------------
-// ## VARIABLES ##
+// ## Variables ##
 dht DHT;
 double incomingAirTemp = 0.0;
 int incomingAirHumidity = 0;
@@ -53,12 +45,30 @@ double returningRoomsAirTemp = 0.0;
 int returningRoomsAirHumidity = 0;
 double afterHeatingCoilTemp = 0.0;
 int afterHeatingCoilHumidity = 0;
-// unsigned long OLED_SCREEN_I2C_ADDRESS = 0x00;
 long timePreviousMeassure = 0;
-int httpSkip = 0;
 
+// --------------------------------------------------------------------------------
+// ## Ethernet config ##
+EthernetClient client;
+byte mac[] = { 0x2E, 0xA8, 0xA9, 0xB5, 0xDA, 0xF6 };
+const char* server  = "192.168.2.35"; // Where stuff is sent at
+const char* api     = "/device/v1/measurements";
+const int port      = 8080;
+const unsigned long HTTP_TIMEOUT = 10000;
+const size_t MAX_CONTENT_SIZE = 1024; // Must be incremented if http response comes out as null
 
-void setup() {  
+// --------------------------------------------------------------------------------
+// ## Task scheduler config ##
+Scheduler runner;
+void dhtRead();
+void httpPostRequest();
+Task t1(11 * 1000, TASK_FOREVER, &dhtRead);
+Task t2(30 * 1000, TASK_FOREVER, &httpPostRequest);    int httpSkip = 0; // Skip some
+
+// --------------------------------------------------------------------------------
+
+// SETUP
+void setup() {
   Serial.begin(9600);
 
   // init ethernet
@@ -69,20 +79,22 @@ void setup() {
     Serial.println(Ethernet.localIP());
   }
 
-  delay(1000);
+  // init task scheduler
+  runner.init();
+  runner.addTask(t1);
+  runner.addTask(t2);
+  t1.enable();
+  t2.enable();
+
+  delay(5 * 1000);
 }
 
+// --------------------------------------------------------------------------------
 
+// LOOP
 void loop() {
-  if (millis() - timePreviousMeassure > 10000) {
-    timePreviousMeassure = millis();
-    dhtRead();
-    httpPostMeasurements();
-    // writeOledScreenText();
-  }
+  runner.execute(); // Runner is responsible for all tasks
 }
-
-
 
 // --------------------------------------------------------------------------------------------------------------------------
 // -------------------------------------A L L - S E N S O R - R E A D I N G - T A S K S -------------------------------------
@@ -134,41 +146,51 @@ void dhtRead() {
 }
 
 
+
 // --------------------------------------------------------------------------------------------------------------------------
 // ----------------------------------------- H T T P - A P I - C A L L - T A S K --------------------------------------------
 // --------------------------------------------------------------------------------------------------------------------------
 
 // Http post operation
-void httpPostMeasurements() {
+void httpPostRequest() {
     if (httpSkip == 0) {
       Serial.print("Http callback");
-      if (connect()) {
+      if (connect(server)) {
+        Serial.print(server); Serial.print(":"); Serial.print(port); Serial.println(api);
         if (sendRequest(server, api) && skipResponseHeaders()) {
+          char response[MAX_CONTENT_SIZE];
+          readReponseContent(response, sizeof(response));
+          parseResponseData(response);
         }
         disconnect();
       }
     }
-    httpSkip = httpSkip + 1;
-    if (httpSkip == 6) {
-      httpSkip = 0; 
+    httpSkip = httpSkip +1;
+    if (httpSkip == 2) {
+      httpSkip = 0;
     };
 }
 
 // Open connection to the HTTP server
-bool connect() {
-  Serial.println("Connecting to server");
-  bool ok = client.connect(server, port);
+bool connect(const char* hostName) {
+  Serial.println("");
+  Serial.print("Connecting to: ");
+  Serial.print(hostName); Serial.print(":"); Serial.print(port);
+  Serial.println("");
+  bool ok = client.connect(hostName, port);
   Serial.println(ok ? "Connected" : "Connection Failed!");
+  Serial.println(client.remoteIP());
   return ok;
 }
 
 // Send the HTTP POST request to the server, same time GET needed response data
 bool sendRequest(const char* host, const char* api) {
-  
+
   // Construct json parameter object
   StaticJsonBuffer<1200> jsonBuffer; // This seems not to affect anywhere
   StaticJsonBuffer<500> jsonBuffer2; // Increment more based on Object count
   JsonObject& root = jsonBuffer.createObject();
+  // root["firmwareBuild"] = firmware_build;
   JsonArray& data = root.createNestedArray("data");
 
   JsonObject& incomingAirObj = jsonBuffer2.createObject();
@@ -177,19 +199,19 @@ bool sendRequest(const char* host, const char* api) {
   incomingAirObj["hum"] = incomingAirHumidity;
 
   JsonObject& outgoingAirToRoomsObj = jsonBuffer2.createObject();
-  incomingAirObj["name"] = "outgoing_air_to_rooms";
-  incomingAirObj["temp"] = outgoingAirToRoomsTemp;
-  incomingAirObj["hum"] = outgoingAirToRoomsHumidity;
+  outgoingAirToRoomsObj["name"] = "outgoing_air_to_rooms";
+  outgoingAirToRoomsObj["temp"] = outgoingAirToRoomsTemp;
+  outgoingAirToRoomsObj["hum"] = outgoingAirToRoomsHumidity;
 
   JsonObject& returningRoomsAirObj = jsonBuffer2.createObject();
-  incomingAirObj["name"] = "returning_rooms_air";
-  incomingAirObj["temp"] = returningRoomsAirTemp;
-  incomingAirObj["hum"] = returningRoomsAirHumidity;
+  returningRoomsAirObj["name"] = "returning_rooms_air";
+  returningRoomsAirObj["temp"] = returningRoomsAirTemp;
+  returningRoomsAirObj["hum"] = returningRoomsAirHumidity;
 
   JsonObject& afterHeatingCoilObj = jsonBuffer2.createObject();
-  incomingAirObj["name"] = "after_heating_coil";
-  incomingAirObj["temp"] = afterHeatingCoilTemp;
-  incomingAirObj["hum"] = afterHeatingCoilHumidity;
+  afterHeatingCoilObj["name"] = "after_heating_coil";
+  afterHeatingCoilObj["temp"] = afterHeatingCoilTemp;
+  afterHeatingCoilObj["hum"] = afterHeatingCoilHumidity;
 
   // Append to data array
   data.add(incomingAirObj);
@@ -209,6 +231,7 @@ bool sendRequest(const char* host, const char* api) {
   client.println(" HTTP/1.1");
   client.print("Host: ");
   client.println(server);
+  //client.println("Content-Type: application/x-www-form-urlencoded");
   client.println("Content-Type: application/json");
   client.println("User-Agent: Arduino/1.0");
   client.println("Connection: close");
@@ -217,10 +240,13 @@ bool sendRequest(const char* host, const char* api) {
   client.println();
   client.print(postData);
   client.println();
-  
+
   return true;
 }
 
+// --------------------------------------------------------------------------------
+
+// Skip HTTP headers so that we are at the beginning of the response's body
 bool skipResponseHeaders() {
   // HTTP headers end with an empty line
   char endOfHeaders[] = "\r\n\r\n";
@@ -232,6 +258,36 @@ bool skipResponseHeaders() {
   return ok;
 }
 
+// --------------------------------------------------------------------------------
+
+// Read the body of the response from the HTTP server
+void readReponseContent(char* content, size_t maxSize) {
+  size_t length = client.readBytes(content, maxSize);
+  content[length] = 0;
+}
+
+// --------------------------------------------------------------------------------
+
+// Parse response data (commands) and apply them
+void parseResponseData(char* content) {
+  StaticJsonBuffer<MAX_CONTENT_SIZE> jsonBuffer; // Increment MAX_CONTENT_SIZE constant variable if repsonse is null
+  JsonObject& root = jsonBuffer.parseObject(content);
+
+  Serial.println("---------- HTTP RESPONSE ----------");
+  String responseData = "";
+  Serial.println(responseData);
+  Serial.println("-----------------------------------");
+
+  if (!root.success()) {
+    Serial.println("JSON parsing failed!");
+    return;
+  }
+
+}
+
+// --------------------------------------------------------------------------------
+
+// Close the connection with the HTTP server
 void disconnect() {
   Serial.println("Disconnect");
   client.stop();
