08d51fd03b479c13e719b66f908d5d4d57a47517
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-11-13 12:07:14 +0200

    add migration to add new fields

--- server/migrations/20211113095639-measurement_add_carbon_dioxide.js
+++ server/migrations/20211113095639-measurement_add_carbon_dioxide.js
@@ -8,6 +8,14 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.addColumn('measurements', 'incoming_air_carbon_dioxide', Sequelize.DECIMAL);
+    await queryInterface.addColumn('measurements', 'incoming_air_carbon_monoxide', Sequelize.DECIMAL);
+    await queryInterface.addColumn('measurements', 'outgoing_air_to_rooms_carbon_dioxide', Sequelize.DECIMAL);
+    await queryInterface.addColumn('measurements', 'outgoing_air_to_rooms_carbon_monoxide', Sequelize.DECIMAL);
+    await queryInterface.addColumn('measurements', 'returning_rooms_air_carbon_dioxide', Sequelize.DECIMAL);
+    await queryInterface.addColumn('measurements', 'returning_rooms_air_carbon_monoxide', Sequelize.DECIMAL);
+    await queryInterface.addColumn('measurements', 'waste_air_out_carbon_dioxide', Sequelize.DECIMAL);
+    await queryInterface.addColumn('measurements', 'waste_air_out_carbon_monoxide', Sequelize.DECIMAL);
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -17,5 +25,13 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
+    await queryInterface.removeColumn('measurements', 'incoming_air_carbon_dioxide', Sequelize.DECIMAL);
+    await queryInterface.removeColumn('measurements', 'incoming_air_carbon_monoxide', Sequelize.DECIMAL);
+    await queryInterface.removeColumn('measurements', 'outgoing_air_to_rooms_carbon_dioxide', Sequelize.DECIMAL);
+    await queryInterface.removeColumn('measurements', 'outgoing_air_to_rooms_carbon_monoxide', Sequelize.DECIMAL);
+    await queryInterface.removeColumn('measurements', 'returning_rooms_air_carbon_dioxide', Sequelize.DECIMAL);
+    await queryInterface.removeColumn('measurements', 'returning_rooms_air_carbon_monoxide', Sequelize.DECIMAL);
+    await queryInterface.removeColumn('measurements', 'waste_air_out_carbon_dioxide', Sequelize.DECIMAL);
+    await queryInterface.removeColumn('measurements', 'waste_air_out_carbon_monoxide', Sequelize.DECIMAL);
   }
 };
